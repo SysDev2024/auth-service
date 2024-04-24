61052a882d201a0ff76370fffa7847e5299ff0af
Magnus411 <magnusgjerstad00@gmail.com>
2024-04-24 13:10:21 +0200

    fix: :bug: Fixing small merge import error

--- src/main/java/no/ntnu/authService/controller/auth/AuthController.java
+++ src/main/java/no/ntnu/authService/controller/auth/AuthController.java
@@ -1,5 +1,6 @@
 package no.ntnu.authService.controller.auth;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;

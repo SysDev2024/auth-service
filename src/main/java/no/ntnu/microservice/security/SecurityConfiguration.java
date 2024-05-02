320bb417922c8b048b17ccca8282dbaf2821a02e
Magnus411 <magnusgjerstad00@gmail.com>
2024-05-02 09:56:01 +0200

    moving jwt filtering to gateway

--- src/main/java/no/ntnu/microservice/security/SecurityConfiguration.java
+++ src/main/java/no/ntnu/microservice/security/SecurityConfiguration.java
@@ -7,15 +7,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
-import no.ntnu.microservice.security.JwtAuthentificationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -26,25 +23,25 @@ public class SecurityConfiguration {
 
         private final AuthenticationProvider authenticationProvider;
 
+        /**
+         * Configures the security filter chain that. The JWT authenficiation filter is
+         * done globaly in the API Gateway. Leaving room for additional security
+         * measures with internal filtering for each service
+         * 
+         * 
+         */
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
                 http
                                 .cors(customizer -> customizer.configurationSource(corsConfigurationSource()))
-
                                 .csrf(csrf -> csrf.disable())
                                 .authorizeHttpRequests((authz) -> authz
-                                                .requestMatchers("/auth/**").permitAll()
-                                                .anyRequest().authenticated())
-                                .sessionManagement((session) -> session
-                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                                .authenticationProvider(authenticationProvider)
-                                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                                                .anyRequest().permitAll());
 
                 return http.build();
         }
 
-        // TODO Use more restrictive settings in production
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
@@ -52,10 +49,8 @@ public class SecurityConfiguration {
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Auth-Token"));
                 configuration.setExposedHeaders(Arrays.asList("X-Auth-Token"));
-                configuration.setAllowCredentials(true); // Only set this if you need to send cookies or authorization
-                                                         // headers with cross-origin requests
-                configuration.setMaxAge(3600L); // Set how long the response from a pre-flight request can be cached by
-                                                // clients
+                configuration.setAllowCredentials(true);
+                configuration.setMaxAge(3600L);
 
                 UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
                 source.registerCorsConfiguration("/**", configuration);
